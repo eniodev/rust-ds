a47754fb4a7d64822472e8b364c3132427cfdbd9
eniodev <enioblvck@gmail.com>
2024-08-09 01:53:29 +0100

    chore: diff between str and String vectors

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ fn generate_vector_from_input() -> Vec<Vec<usize>> {
 
 // 0.1 Write a Rust program to capitalize the first character of each element of a given string vector. 
 // Return the vector.
-
 fn capitalize_the_first_letter(list:Vec<String>)-> () {
     for el in list {
         println!("{}", el);
@@ -31,6 +30,24 @@ fn main() {
     // let matrix:Vec<Vec<usize>> = generate_vector_from_input(); 
     // println!("{:?}", matrix);
     
-    let mut list: Vec<String> = vec!["-w".to_string(), "60".to_string(), "args".to_string()];
-    capitalize_the_first_letter(list)
+    //let mut list: Vec<String> = vec!["-w".to_string(), "60".to_string(), "args".to_string()];
+    //capitalize_the_first_letter(list) [unfinished]
+    
+    slice_arr_to_string_arr();
 }
+
+
+// Manually copy a slice array to a String array
+fn slice_arr_to_string_arr() -> () {
+    let string_arr: [&str; 3] = ["hello", "from", "porto"];
+    for el in string_arr {
+        print!("{} ", el);
+    }
+    
+    let new_string_arr: Vec<String> = string_arr.into_iter().map(|el| el.to_owned()).collect();
+    
+    for el in new_string_arr.into_iter().enumerate() {
+        print!("{:?} ", el)
+    }
+}
+
